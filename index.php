ff2fd338a90b14859d239cc7a19899182ab3ca5c
Iunora93 <eleonoraruggieri22@gmail.com>
2022-12-15 16:12:17 +0100

    Aggiunge file showPassword

--- index.php
+++ index.php
@@ -1,52 +1,4 @@
-<?php 
 
-function generatePassword() {
-    $min_length=8;  //Minimum length of the password
-    $min_numbers=2; //Minimum of numbers AND special characters
-    $min_letters=2; //Minimum of letters
-    
-    $password = '';
-    $numbers=0;
-    $letters=0;
-    $length=0;
-    
-    while ( $length <= $min_length OR $numbers <= $min_numbers OR $letters <= $min_letters) {
-        $length+=1;
-        $type=rand(1, 3);
-        if ($type==1) {
-            $password .= chr(rand(33, 64)); //Numbers and special characters
-            $numbers+=1;
-        }elseif ($type==2) {
-            $password .= chr(rand(65, 90)); //A->Z
-            $letters+=1;
-        }else {
-            $password .= chr(rand(97, 122)); //a->z
-            $letters+=1;
-        }
-        
-    
-    } 
-    return $password;
-   
-    }
-    var_dump(generatePassword())  ; 
-    echo (generatePassword());
-    if( !empty($_GET['password']) && !empty($_GET['password']) ) {
-        $credentials = [
-            "password" => $_GET['password'],
-        ];
-    
-        $user = (generatePassword());
-    
-        if( $user ) {
-            $_SESSION['logged_in'] = true;
-            
-        } else {
-            $_SESSION['error'] = "Credenziali errate";
-            
-        }
-    }
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -79,7 +31,7 @@ function generatePassword() {
             </div>
 
             <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3">Accedi</button>
+                <a class="btn btn-primary mb-3" href="showPassword.php">Accedi</a>
             </div>
         </form>
     </body>
