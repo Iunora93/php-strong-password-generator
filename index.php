eaccb7445a35adee40ec201df7965ddb14bc2fcd
Iunora93 <eleonoraruggieri22@gmail.com>
2022-12-15 16:32:03 +0100

    Modifica file index

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
+<?php
 
-
+function generatePassword() {
+    $min_length=8;  //Minimum length of the password
+    $min_numbers=2; //Minimum of numbers AND special characters
+    $min_letters=2; //Minimum of letters
+    
+    $password = '';
+    $numbers=0;
+    $letters=0;
+    $length=0;
+    
+    while ( $length <= $min_length OR $numbers <= $min_numbers OR $letters <= $min_letters) {
+        $length+=1;
+        $type=rand(1, 3);
+        if ($type==1) {
+            $password .= chr(rand(33, 64)); //Numbers and special characters
+            $numbers+=1;
+        }elseif ($type==2) {
+            $password .= chr(rand(65, 90)); //A->Z
+            $letters+=1;
+        }else {
+            $password .= chr(rand(97, 122)); //a->z
+            $letters+=1;
+        }
+        
+    
+    } 
+    return $password;
+   
+    }
+     
+    $_SESSION = (generatePassword());
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
--- showPassword.php
+++ showPassword.php
@@ -1,50 +1,19 @@
 <?php 
-session_start();
-include_once __DIR__ . "/index.php";
-function generatePassword() {
-    $min_length=8;  //Minimum length of the password
-    $min_numbers=2; //Minimum of numbers AND special characters
-    $min_letters=2; //Minimum of letters
-    
-    $password = '';
-    $numbers=0;
-    $letters=0;
-    $length=0;
-    
-    while ( $length <= $min_length OR $numbers <= $min_numbers OR $letters <= $min_letters) {
-        $length+=1;
-        $type=rand(1, 3);
-        if ($type==1) {
-            $password .= chr(rand(33, 64)); //Numbers and special characters
-            $numbers+=1;
-        }elseif ($type==2) {
-            $password .= chr(rand(65, 90)); //A->Z
-            $letters+=1;
-        }else {
-            $password .= chr(rand(97, 122)); //a->z
-            $letters+=1;
-        }
-        
-    
-    } 
-    return $password;
-   
-    }
-    var_dump(generatePassword())  ; 
-    echo (generatePassword());
+    session_start();
+    include_once __DIR__ . "/index.php";
     if( !empty($_GET['password']) && !empty($_GET['password']) ) {
         $credentials = [
             "password" => $_GET['password'],
         ];
-    
+
         $user = (generatePassword());
-    
+
         if( $user ) {
             $_SESSION['logged_in'] = true;
+            header("Location: http://localhost:8888/php-strong-password/showPassword.php");
+        die();
             
-        } else {
-            $_SESSION['error'] = "Credenziali errate";
-            
-        }
+        } 
     }
+    echo (generatePassword());
 ?>
